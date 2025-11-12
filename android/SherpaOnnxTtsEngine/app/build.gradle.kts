29b230091f80ea4335a3cf913bf75c3a05af9b4f
EMbanAPHe <rabanastrian@hotmail.com>
2025-11-13 09:31:43 +1100

    Update build.gradle.kts

--- android/SherpaOnnxTtsEngine/app/build.gradle.kts
+++ android/SherpaOnnxTtsEngine/app/build.gradle.kts
@@ -62,7 +62,6 @@ android {
 
 dependencies {
 
-    implementation("com.k2fsa.sherpa.onnx:sherpa-onnx-android:1.12.11")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
